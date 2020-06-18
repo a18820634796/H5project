c5ccf6c496056c1704ac880b19b0d1cb3ee137e8
liangwenjun <wj18820634796@163.com>
2020-06-18 21:59:25 +0800

    使用svg作为返回按钮

--- src/page/mainModules/search.vue
+++ src/page/mainModules/search.vue
@@ -1,22 +1,29 @@
 <template>
-    <div class="#search">
-        <p class="goBack"
-           @click="$router.go(-1)">
-            <span>返回</span>
+    <div class="search">
+        <p class="head" @click="$router.go(-1)">
+            <span class="goBack"></span>
         </p>
     </div>
 </template>
 <style lang="scss" scoped>
-.search {
-    width: 100%;
-    height: 100%;
-    .goBack {
+    .search {
         width: 100%;
-        height: 30px;
-        background: chartreuse;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
+        height: 100%;
+
+        .head {
+            width: 100%;
+            height: 30px;
+            background: rgba(172, 172, 172, 0.3);
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+
+            .goBack {
+                width: 20px;
+                height: 20px;
+                background: url('../../assets/icons/chevron-left.svg') no-repeat;
+                background-size: 100% 100%;
+            }
+        }
     }
-}
 </style>
